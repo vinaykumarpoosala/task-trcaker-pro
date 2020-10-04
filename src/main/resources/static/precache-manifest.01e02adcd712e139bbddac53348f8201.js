4fea921ef4e6619a6d3f1af7d1f6652c5b6bde80
= <->
2020-10-04 11:33:33 +0530

    raect build added

---
+++ src/main/resources/static/precache-manifest.01e02adcd712e139bbddac53348f8201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff718b78c845fa7e6ed888c36b02d322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6740f034d9debcab9476",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3951ca2af2298de91cf8",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "6740f034d9debcab9476",
+    "url": "/static/js/2.01b2606b.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/static/js/2.01b2606b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3951ca2af2298de91cf8",
+    "url": "/static/js/main.619c129c.chunk.js"
+  },
+  {
+    "revision": "62ea86c90133b44131f0",
+    "url": "/static/js/runtime-main.6b804cdb.js"
+  }
+]);
